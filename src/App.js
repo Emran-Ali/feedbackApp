1e3dd279d037ad4d4eba0a44f774389727ee1f42
Emran <emranitmbstu@gmail.com>
2023-08-18 19:07:40 +0600

    Add about page & Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './Data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
+import About from './pages/About'
 
 
 
@@ -26,17 +28,19 @@ function App() {
         }
     }
     return (
-        <>
+        <Router>
             <Header text="Color me As You can" />
-
             <div className="container">
                 <FeedbackForm handleAdd={addFeedback} />
                 <FeedbackStats feedback={feedback} />
                 <FeedbackList feedback={feedback}
                     handleDelete={deleteFeedback} />
-            </div>
-        </>
 
+                <Routes>
+                    <Route exact path='/about' Component={About}> </Route>
+                </Routes>
+            </div>
+        </ Router >
     )
 }
 
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 import FeedbackItem from "./FeedbackItem"
+import { AnimatePresence, motion } from "framer-motion"
 import Card from './Shared/Card'
 import PropTypes from 'prop-types'
 
@@ -14,14 +15,23 @@ function FeedbackList({ feedback, handleDelete }) {
 
     return (
         <div className="feedback-list">
-            {
-
-                feedback.map((item) => (
-                    <FeedbackItem key={item.id} item={item}
-                        handleDelete={handleDelete} />
-                ))
-            }
-
+            <AnimatePresence>
+                {
+                    feedback.map((item) => (
+                        <motion.div
+                            key={item.id}
+                            whileHover={{ scale: 1.1 }}
+                            // whileTap={{ scale: 1.1 }}
+                            drag="x"
+                            dragConstraints={{ left: -50, right: 50 }}
+                        >
+                            <FeedbackItem key={item.id}
+                                item={item}
+                                handleDelete={handleDelete} />
+                        </motion.div>
+                    ))
+                }
+            </AnimatePresence >
         </div>
     )
 }
