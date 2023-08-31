437727bd1e44a803353f1a30e818f9a0489fc3b3
Emran <emranitmbstu@gmail.com>
2023-08-31 17:48:40 +0600

    add json data for bakend, rest API

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,25 +1,24 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
 const FeedbackContext = createContext();
 export const FeedbackProvider = ({ children }) => {
 
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: "this is from context provider",
-      rating: 10,
-    },
-    {
-      id: 2,
-      text: "this is from context provider 2",
-      rating: 9,
-    }
-  ])
+  const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setEditFeedback] = useState({
     item: {},
     edit: false,
   })
+  useEffect(() => {
+    fetchFeedback();
+
+  }, [])
+
+  const fetchFeedback = async () => {
+    const res = await fetch(`http://localhost:5000/feedback?_sort=id`);
+    const data = await res.json();
+    setFeedback(data);
+  }
   const updateFeedback = (id, updFeedback) => {
     setFeedback(
       feedback.map((item) =>
